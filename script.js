64e271e9b235d8da78563d5973885e19be71d311
niina311 <marina.cardoso.rodrigues@escola.pr.gov.br>
2025-08-21 08:45:20 -0300

    tny fuyjt

---
+++ script.js
@@ -0,0 +1,17 @@
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+
+botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
+
+function mostraPalavrasChave() {
+  const texto = document.querySelector("#entrada-de-texto").value;
+
+  const campoResultado = document.querySelector("#resultado-palavrachave");
+
+  const palavras = texto.split(" ");
+
+  campoResultado.textContent = palavras.join(", ")
+}
+function processaTexto(texto) {
+    let palavras = texto.split(/\P{L}+/u);
+    return palavras;
+}
